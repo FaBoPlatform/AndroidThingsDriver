e8a4eb8ee6bbe3cefe0eeda8b3a023ffdf7c8485
Akira Sasaki <gclue.akira@gmail.com>
2018-11-16 16:31:46 +0900

    Add MPL115.

---
+++ driver/src/main/java/io/fabo/driver/MPL115.java
@@ -0,0 +1,145 @@
+package io.fabo.driver;
+
+import android.support.annotation.IntDef;
+
+import com.google.android.things.pio.I2cDevice;
+import com.google.android.things.pio.PeripheralManager;
+
+import java.io.IOException;
+
+public class MPL115 implements AutoCloseable {
+    private static final String TAG = MPL115.class.getSimpleName();
+
+    /**
+     * I2C slave address of the MPL115.
+     */
+    public static final int I2C_ADDRESS = 0x60;
+
+    // Register.
+    /** 10-bit Pressure ADC output value MSB. */
+    private final int REG_PADC_MSB = 0x00;
+    /** 10-bit Pressure ADC output value LSB. */
+    private final int REG_PADC_LSB = 0x01;
+    /** 10-bit Temperature ADC output value MSB. */
+    private final int REG_TADC_MSB = 0x02;
+    /** 10-bit Temperature ADC output value LSB. */
+    private final int REG_TACD_LSB = 0x03;
+    /** a0 coefficient MSB. */
+    private final int REG_A0_MSB = 0x04;
+    /** a0 coefficient LSB. */
+    private final int REG_A0_LSB = 0x05;
+    /** b1 coefficient MSB. */
+    private final int REG_B1_MSB = 0x06;
+    /** b1 coefficient LSB. */
+    private final int REG_B1_LSB = 0x07;
+    /** b2 coefficient MSB. */
+    private final int REG_B2_MSB = 0x08;
+    /** b2 coefficient LSB. */
+    private final int REG_B2_LSB = 0x09;
+    /** b2 coefficient MSB. */
+    private final int REG_C12_MSB = 0x0A;
+    /** b2 coefficient LSB. */
+    private final int REG_C12_LSB = 0x0B;
+    /** Start Pressure and Temperature Conversion. */
+    private final int REG_CONVERT = 0x12;
+
+    private static float mA0;
+    private static float mB1;
+    private static float mB2;
+    private static float mC12;
+
+
+    private I2cDevice mDevice;
+
+    /**
+     * Create a new MPL115 driver connected to the given I2C bus.
+     * @param bus
+     * @throws IOException
+     */
+    public MPL115(String bus) throws IOException {
+        PeripheralManager pioService = PeripheralManager.getInstance();
+        I2cDevice device = pioService.openI2cDevice(bus, I2C_ADDRESS);
+        try {
+            connect(device);
+        } catch (IOException|RuntimeException e) {
+            try {
+                close();
+            } catch (IOException|RuntimeException ignored) {
+            }
+            throw e;
+        }
+    }
+
+    /**
+     * Create a new MPL115 driver connected to the given I2C device.
+     * @param device
+     * @throws IOException
+     */
+    /*package*/ MPL115(I2cDevice device) throws IOException {
+        connect(device);
+    }
+
+    private void connect(I2cDevice device) throws IOException {
+        if (mDevice != null) {
+            throw new IllegalStateException("device already connected");
+        }
+        mDevice = device;
+    }
+
+
+    /**
+     * Close the driver and the underlying device.
+     */
+    @Override
+    public void close() throws IOException {
+        if (mDevice != null) {
+            try {
+                mDevice.close();
+            } finally {
+                mDevice = null;
+            }
+        }
+    }
+
+    /**
+     @brief Read Coefficient Data
+     */
+    public void readCoef() {
+        try {
+            byte data_buff[] = new byte[8];
+            mDevice.readRegBuffer(REG_A0_MSB, data_buff, data_buff.length);
+            mA0  = ((float) ((data_buff[0] << 8) + data_buff[1]) / ((long)1 << 3));
+            mB1  = ((float) ((data_buff[2] << 8) + data_buff[3]) / ((long)1 << 13));
+            mB2  = ((float) ((data_buff[4] << 8) + data_buff[5]) / ((long)1 << 14));
+            mC12 = ((float) ((data_buff[6] << 8) + data_buff[7]) / ((long)1 << 24));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Read an accelerometer sample.
+     * @return hpa, temp.
+     * @throws IOException
+     * @throws IllegalStateException
+     */
+    public float[] readData() throws IOException, IllegalStateException {
+        if (mDevice == null) {
+            throw new IllegalStateException("device not connected");
+        }
+        mDevice.writeRegByte(REG_CONVERT, (byte)0x01);
+
+        byte data_buff[] = new byte[4];
+        mDevice.readRegBuffer(REG_PADC_MSB, data_buff, data_buff.length);
+
+        int padc = (((data_buff[0] & 0xff) << 8) | (data_buff[1] & 0xff)) >> 6;
+        int tadc = (((data_buff[2] & 0xff) << 8) | (data_buff[3] & 0xff)) >> 6;
+
+        float pcomp = mA0 + (mB1 + mC12 * tadc) * padc + mB2 * tadc;
+        float hpa = (float) (pcomp * ((1150.0 - 500.0) / 1023.0) + 500.0);
+        float temp = (float) (25.0 - ((float) tadc - 512.0) / 5.35);
+
+        return new float[]{hpa, temp};
+    }
+
+}
---
+++ driver/src/main/java/io/fabo/driver/MPL115BarometerDriver.java
@@ -0,0 +1,98 @@
+package io.fabo.driver;
+
+import android.hardware.Sensor;
+
+import com.google.android.things.userdriver.UserDriverManager;
+import com.google.android.things.userdriver.sensor.UserSensor;
+import com.google.android.things.userdriver.sensor.UserSensorDriver;
+import com.google.android.things.userdriver.sensor.UserSensorReading;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class MPL115BarometerDriver implements AutoCloseable {
+
+    private static final String TAG = MPL115BarometerDriver.class.getSimpleName();
+    private static final String DRIVER_NAME = "FaBoMPL115";
+    private static final String DRIVER_VENDOR = "FaBo";
+    private static final int DRIVER_VERSION = 1;
+    private MPL115 mDevice;
+    private UserSensor mUserSensor;
+
+    /**
+     * Create a new framework accelerometer driver connected to the given I2C bus.
+     * The driver emits {@link Sensor} with acceleration data when registered.
+     * @param bus
+     * @throws IOException
+     * @see #register()
+     */
+    public MPL115BarometerDriver(String bus) throws IOException {
+        mDevice = new MPL115(bus);
+    }
+
+    /**
+     * Close the driver and the underlying device.
+     * @throws IOException
+     */
+    @Override
+    public void close() throws IOException {
+        unregister();
+        if (mDevice != null) {
+            try {
+                mDevice.close();
+            } finally {
+                mDevice = null;
+            }
+        }
+    }
+
+    /**
+     * Register the driver in the framework.
+     * @see #unregister()
+     */
+    public void register() {
+        if (mDevice == null) {
+            throw new IllegalStateException("cannot registered closed driver");
+        }
+        if (mUserSensor == null) {
+            mUserSensor = build(mDevice);
+            UserDriverManager.getInstance().registerSensor(mUserSensor);
+        }
+    }
+
+    /**
+     * Unregister the driver from the framework.
+     */
+    public void unregister() {
+        if (mUserSensor != null) {
+            UserDriverManager.getInstance().unregisterSensor(mUserSensor);
+            mUserSensor = null;
+        }
+    }
+
+    static UserSensor build(final MPL115 mpl115) {
+        return new UserSensor.Builder()
+                .setType(Sensor.TYPE_LIGHT)
+                .setName(DRIVER_NAME)
+                .setVendor(DRIVER_VENDOR)
+                .setVersion(DRIVER_VERSION)
+                .setUuid(UUID.randomUUID())
+                .setDriver(new UserSensorDriver() {
+                    @Override
+                    public UserSensorReading read() throws IOException {
+                        float data[] = mpl115.readData();
+                        return new UserSensorReading(data);
+                    }
+
+                    @Override
+                    public void setEnabled(boolean enabled) throws IOException {
+                        if (enabled) {
+                            mpl115.readCoef();
+                        } else {
+                            // ToDo
+                        }
+                    }
+                })
+                .build();
+    }
+}
